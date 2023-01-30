a640b26403101efc022821163db35abdfb88c4f4
marco.spasiano <marco.spasiano@cnr.it>
2023-01-30 10:36:42 +0100

    In caso di Terzo di tipo studio associato legato ad un contratto, è possibile associare ad un ordine quel contratto specificando poi uno dei terzi legati allo studio associato.

--- sigla-ejb/src/main/java/it/cnr/contab/anagraf00/core/bulk/Anagrafico_terzoBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/anagraf00/core/bulk/Anagrafico_terzoBulk.java
@@ -17,107 +17,101 @@
 
 package it.cnr.contab.anagraf00.core.bulk;
 
-import it.cnr.contab.compensi00.docs.bulk.V_terzo_per_compensoBulk;
+import java.util.Optional;
 
 /**
  * Gestione dei dati relativi alla tabella Anagrafico/Terzo
  */
 
 public class Anagrafico_terzoBulk extends Anagrafico_terzoBase {
-	private AnagraficoBulk anagrafico;
-	private V_persona_fisicaBulk terzo;
+    public static String LEGAME_STUDIO_ASSOCIATO = "STASS";
+    private AnagraficoBulk anagrafico;
+    private V_anagrafico_terzoBulk terzo;
 
-	public static String LEGAME_STUDIO_ASSOCIATO = "STASS";
-	
-	public Anagrafico_terzoBulk() {
-		super();
-	}
-	
-	public Anagrafico_terzoBulk(java.lang.Integer cd_anag, java.lang.Integer cd_terzo, java.lang.String ti_legame) {
-		super(cd_anag, cd_terzo, ti_legame);
-	}
+    public Anagrafico_terzoBulk() {
+        super();
+    }
 
-	/**
-	 * Restituisce l'<code>AnagraficoBulk</code> a cui l'oggetto è correlato.
-	 *
-	 * @return it.cnr.contab.anagraf00.core.bulk.AnagraficoBulk
-	 *
-	 * @see setAnagrafico
-	 */
+    public Anagrafico_terzoBulk(java.lang.Integer cd_anag, java.lang.Integer cd_terzo, java.lang.String ti_legame) {
+        super(cd_anag, cd_terzo, ti_legame);
+    }
 
-	public AnagraficoBulk getAnagrafico() {
-		return anagrafico;
-	}
+    /**
+     * Restituisce l'<code>AnagraficoBulk</code> a cui l'oggetto è correlato.
+     *
+     * @return it.cnr.contab.anagraf00.core.bulk.AnagraficoBulk
+     * @see setAnagrafico
+     */
 
-	/**
-	 * Restituisce l'<code>V_persona_fisicaBulk</code> a cui l'oggetto è correlato.
-	 *
-	 * @return it.cnr.contab.anagraf00.core.bulk.TerzoBulk
-	 *
-	 * @see setTerzo
-	 */
+    public AnagraficoBulk getAnagrafico() {
+        return anagrafico;
+    }
 
-	public V_persona_fisicaBulk getTerzo() {
-		return terzo;
-	}
+    /**
+     * Imposta l'<code>AnagraficoBulk</code> a cui l'oggetto è correlato.
+     *
+     * @param newAnagrafico Anagrafica di riferimento.
+     * @see getAnagrafico
+     */
 
-	/**
-	 * Imposta l'<code>AnagraficoBulk</code> a cui l'oggetto è correlato.
-	 *
-	 * @param newAnagrafico Anagrafica di riferimento.
-	 *
-	 * @see getAnagrafico
-	 */
+    public void setAnagrafico(AnagraficoBulk newAnagrafico) {
+        anagrafico = newAnagrafico;
+    }
 
-	public void setAnagrafico(AnagraficoBulk newAnagrafico) {
-		anagrafico = newAnagrafico;
-	}
+    /**
+     * Restituisce l'<code>V_persona_fisicaBulk</code> a cui l'oggetto è correlato.
+     *
+     * @return it.cnr.contab.anagraf00.core.bulk.TerzoBulk
+     * @see setTerzo
+     */
 
-	/**
-	 * Imposta l'<code>V_persona_fisicaBulk</code> a cui l'oggetto è correlato.
-	 *
-	 * @param newTerzo Terzo di riferimento.
-	 *
-	 * @see getTerzo
-	 */
+    public V_anagrafico_terzoBulk getTerzo() {
+        return terzo;
+    }
 
-	public void setTerzo(V_persona_fisicaBulk newTerzo) {
-		terzo = newTerzo;
-	}
-	
-	@Override
-	public Integer getCd_anag() {
-		if (getAnagrafico()==null) return null;
-		return getAnagrafico().getCd_anag();
-	}
-	
-	@Override
-	public Integer getCd_terzo() {
-		if (getTerzo()==null) return null;
-		return getTerzo().getCd_terzo();
-	}
-	
-	@Override
-	public void setCd_anag(Integer cd_anag) {
-		if (getAnagrafico()!=null) 
-			getAnagrafico().setCd_anag(cd_anag);
-	}	
+    /**
+     * Imposta l'<code>V_persona_fisicaBulk</code> a cui l'oggetto è correlato.
+     *
+     * @param newTerzo Terzo di riferimento.
+     * @see getTerzo
+     */
 
-	@Override
-	public void setCd_terzo(Integer cd_terzo) {
-		if (getTerzo()!=null) 
-			getTerzo().setCd_terzo(cd_terzo);
-	}	
+    public void setTerzo(V_anagrafico_terzoBulk newTerzo) {
+        terzo = newTerzo;
+    }
 
-	/**
-	 * Restituisce il valore della proprietà 'ds_terzo'
-	 *
-	 * @return Il valore della proprietà 'ds_terzo'
-	 */
-	public java.lang.String getDs_terzo() {
-		if ( getTerzo() != null && getTerzo().getAnagrafico() != null &&
-		     getTerzo().getAnagrafico().getCognome()!=null )
-			return getTerzo().getAnagrafico().getCognome() + " " + getTerzo().getAnagrafico().getNome();
-		return "";	
-	}
+    @Override
+    public Integer getCd_anag() {
+        if (getAnagrafico() == null) return null;
+        return getAnagrafico().getCd_anag();
+    }
+
+    @Override
+    public void setCd_anag(Integer cd_anag) {
+        if (getAnagrafico() != null)
+            getAnagrafico().setCd_anag(cd_anag);
+    }
+
+    @Override
+    public Integer getCd_terzo() {
+        if (getTerzo() == null) return null;
+        return getTerzo().getCd_terzo();
+    }
+
+    @Override
+    public void setCd_terzo(Integer cd_terzo) {
+        if (getTerzo() != null)
+            getTerzo().setCd_terzo(cd_terzo);
+    }
+
+    /**
+     * Restituisce il valore della proprietà 'ds_terzo'
+     *
+     * @return Il valore della proprietà 'ds_terzo'
+     */
+    public java.lang.String getDs_terzo() {
+        return Optional.ofNullable(getTerzo())
+                .flatMap(vAnagraficoTerzoBulk -> Optional.ofNullable(vAnagraficoTerzoBulk.getDenominazione_sede()))
+                .orElse("");
+    }
 }
