bb08905100bb61b1271e9eaea3bb2d2680606e4c
marco.spasiano <marco.spasiano@cnr.it>
2023-02-10 12:20:02 +0100

    FIX Rest for session tracer

--- sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/IteratorTracersResource.java
+++ sigla-web/src/main/java/it/cnr/contab/web/rest/resource/util/IteratorTracersResource.java
@@ -17,6 +17,7 @@
 
 package it.cnr.contab.web.rest.resource.util;
 
+import it.cnr.contab.utenze00.bp.CNRUserContext;
 import it.cnr.contab.web.rest.local.util.IteratorTracersLocal;
 import it.cnr.jada.util.ejb.EJBTracer;
 import org.slf4j.Logger;
@@ -26,9 +27,12 @@ import javax.ejb.Stateless;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import java.time.format.DateTimeFormatter;
+import java.util.AbstractMap;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Stateless
 public class IteratorTracersResource implements IteratorTracersLocal {
@@ -42,6 +46,18 @@ public class IteratorTracersResource implements IteratorTracersLocal {
                 values
                         .parallelStream()
                         .sorted((iteratorTracer, t1) -> iteratorTracer.getCreationDate().compareTo(t1.getCreationDate()))
+                        .map(iteratorTracer -> {
+                            return Stream.of(
+                                            new AbstractMap.SimpleEntry<>("date", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(iteratorTracer.getCreationDate())),
+                                            new AbstractMap.SimpleEntry<>("query", iteratorTracer.getQuery()),
+                                            new AbstractMap.SimpleEntry<>("user", iteratorTracer.getUserContext().getUser()),
+                                            new AbstractMap.SimpleEntry<>("esercizio", CNRUserContext.getEsercizio(iteratorTracer.getUserContext())),
+                                            new AbstractMap.SimpleEntry<>("cds", CNRUserContext.getCd_cds(iteratorTracer.getUserContext())),
+                                            new AbstractMap.SimpleEntry<>("uo", CNRUserContext.getCd_unita_organizzativa(iteratorTracer.getUserContext())),
+                                            new AbstractMap.SimpleEntry<>("cdr", CNRUserContext.getCd_cdr(iteratorTracer.getUserContext())),
+                                            new AbstractMap.SimpleEntry<>("sessionId", iteratorTracer.getUserContext().getSessionId()))
+                                    .collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
+                        })
                         .collect(Collectors.toList()))
         ).build();
     }
