4c04bac0f48e97970273aca35617bc5b69f964f8
cirosalvio <cirosalvio@hotmail.com>
2023-03-14 19:22:46 +0100

    Implpememtazione Stato assegnato a cds di default per un sospeso

--- sigla-ejb/src/main/java/it/cnr/contab/config00/ejb/Configurazione_cnrComponentSessionBean.java
+++ sigla-ejb/src/main/java/it/cnr/contab/config00/ejb/Configurazione_cnrComponentSessionBean.java
@@ -945,4 +945,24 @@ public class Configurazione_cnrComponentSessionBean extends it.cnr.jada.ejb.Gene
         }
     }
 
+    @Override
+    public Boolean isGestioneStatoInizialeSospesiAttivo(UserContext param0) throws ComponentException, RemoteException {
+        try {
+            Boolean result = componentObj.isGestioneStatoInizialeSospesiAttivo(param0);
+            component_invocation_succes(param0, componentObj);
+            return result;
+        } catch (it.cnr.jada.comp.NoRollbackException e) {
+            component_invocation_succes(param0, componentObj);
+            throw e;
+        } catch (ComponentException e) {
+            component_invocation_failure(param0, componentObj);
+            throw e;
+        } catch (RuntimeException e) {
+            throw uncaughtRuntimeException(param0, componentObj, e);
+        } catch (Error e) {
+            throw uncaughtError(param0, componentObj, e);
+        }
+    }
+
+
 }
