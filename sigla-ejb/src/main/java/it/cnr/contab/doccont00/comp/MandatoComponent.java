895674d358f489823606a9c63886779e1f180747
cirosalvio <cirosalvio@hotmail.com>
2023-04-26 05:35:27 +0200

    Fix issue creazione mandati con modalità pagamento DEL ( Delegato) sul documento ammnistrativo

--- sigla-ejb/src/main/java/it/cnr/contab/doccont00/comp/MandatoComponent.java
+++ sigla-ejb/src/main/java/it/cnr/contab/doccont00/comp/MandatoComponent.java
@@ -5907,6 +5907,15 @@ public class MandatoComponent extends ScritturaPartitaDoppiaFromDocumentoCompone
                         return mandatoRigaComplete
                                 .getDocammRighe()
                                 .stream()
+                                .filter( iDocumentoAmministrativoRigaBulk -> {
+                                    try {
+                                        Rif_modalita_pagamentoBulk r = ((Rif_modalita_pagamentoBulk)getHome(userContext, Rif_modalita_pagamentoBulk.class).findByPrimaryKey(new Rif_modalita_pagamentoBulk(iDocumentoAmministrativoRigaBulk.getModalita_pagamento().getCd_modalita_pag())));
+                                        return (!r.getFl_per_cessione());
+                                    } catch (PersistencyException | ComponentException e) {
+                                        // va in errore l'interfaccia nel caso di modalità pagamento DEL che deve essere gestita
+                                        return true;
+                                    }
+                                })
                                 .filter(iDocumentoAmministrativoRigaBulk -> {
                                     return !iDocumentoAmministrativoRigaBulk.getBanca().equalsByPrimaryKey(bancaBulk) ||
                                             !iDocumentoAmministrativoRigaBulk.getModalita_pagamento().equalsByPrimaryKey(rifModalitaPagamentoBulk);
