326b4b36b8fd1993873cbfe2b8e31a588a0e1c0d
Piergiorgio Faraglia <p.faraglia@gmail.com>
2023-10-24 12:53:48 +0200

    Aggiunge gestione dei campi voceDa e voceA come searchtool

---
+++ sigla-ejb/src/main/java/it/cnr/contab/prevent01/bulk/StampaMastroRendicontoFinanziarioHome.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2019  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.contab.prevent01.bulk;
+
+import it.cnr.contab.config00.pdcfin.bulk.Elemento_voceBulk;
+import it.cnr.contab.config00.pdcfin.bulk.Elemento_voceHome;
+import it.cnr.contab.utenze00.bp.CNRUserContext;
+import it.cnr.jada.UserContext;
+import it.cnr.jada.bulk.BulkHome;
+import it.cnr.jada.comp.ComponentException;
+import it.cnr.jada.persistency.PersistencyException;
+import it.cnr.jada.persistency.PersistentCache;
+import it.cnr.jada.persistency.sql.CompoundFindClause;
+import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.sql.Connection;
+
+/**
+ * Insert the type's description here.
+ * Creation date: (24/10/2023 12.20.00)
+ * @author: Piergiorgio Faraglia
+ */
+public class StampaMastroRendicontoFinanziarioHome extends BulkHome {
+
+	public StampaMastroRendicontoFinanziarioHome(Class clazz, java.sql.Connection conn) {
+		super(clazz, conn);
+	}
+
+	public StampaMastroRendicontoFinanziarioHome(Class clazz, java.sql.Connection conn, it.cnr.jada.persistency.PersistentCache persistentCache) {
+		super(clazz, conn, persistentCache);
+	}
+
+	public StampaMastroRendicontoFinanziarioHome(Connection conn, PersistentCache persistentCache) {
+		super(StampaMastroRendicontoFinanziarioBulk.class, conn, persistentCache);
+	}
+
+	public SQLBuilder selectVoce_daByClause(UserContext userContext, StampaMastroRendicontoFinanziarioBulk parametri,
+										   Elemento_voceHome elementoVoceHome, Elemento_voceBulk elementoVoceBulk,
+										   CompoundFindClause compoundfindclause) throws PersistencyException, ComponentException {
+		SQLBuilder sql = elementoVoceHome.createSQLBuilder();
+		sql.addSQLClause("AND","ESERCIZIO",sql.EQUALS, CNRUserContext.getEsercizio(userContext));
+		sql.addSQLClause("AND","TI_GESTIONE", sql.EQUALS, parametri.getTi_gestione());
+		sql.addOrderBy("CD_ELEMENTO_VOCE");
+		sql.addClause( compoundfindclause );
+		return sql;
+	}
+
+	public SQLBuilder selectVoce_aByClause(UserContext userContext, StampaMastroRendicontoFinanziarioBulk parametri,
+											Elemento_voceHome elementoVoceHome, Elemento_voceBulk elementoVoceBulk,
+											CompoundFindClause compoundfindclause) throws PersistencyException, ComponentException {
+		SQLBuilder sql = elementoVoceHome.createSQLBuilder();
+		sql.addSQLClause("AND","ESERCIZIO",sql.EQUALS, CNRUserContext.getEsercizio(userContext));
+		sql.addSQLClause("AND","TI_GESTIONE", sql.EQUALS, parametri.getTi_gestione());
+		sql.addOrderBy("CD_ELEMENTO_VOCE");
+		sql.addClause( compoundfindclause );
+		return sql;
+	}
+
+}
