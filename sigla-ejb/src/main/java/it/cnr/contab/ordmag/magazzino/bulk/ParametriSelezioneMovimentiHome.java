ad4e801e47341a912645cb65f5e27f0528162f5d
salvio_ciro <cirosalvio@hotmail.com>
2023-12-02 13:31:53 +0100

    fix segnalazione "Selezionare prima il codice del magazzino"

--- sigla-ejb/src/main/java/it/cnr/contab/ordmag/magazzino/bulk/ParametriSelezioneMovimentiHome.java
+++ sigla-ejb/src/main/java/it/cnr/contab/ordmag/magazzino/bulk/ParametriSelezioneMovimentiHome.java
@@ -20,21 +20,8 @@
  * Date 21/09/2017
  */
 package it.cnr.contab.ordmag.magazzino.bulk;
-import java.sql.Connection;
-import java.util.Optional;
 
-import it.cnr.contab.ordmag.anag00.AbilUtenteUopOperBulk;
-import it.cnr.contab.ordmag.anag00.AbilUtenteUopOperHome;
-import it.cnr.contab.ordmag.anag00.AbilUtenteUopOperMagBulk;
-import it.cnr.contab.ordmag.anag00.AbilUtenteUopOperMagHome;
-import it.cnr.contab.ordmag.anag00.NumerazioneOrdBulk;
-import it.cnr.contab.ordmag.anag00.NumerazioneOrdHome;
-import it.cnr.contab.ordmag.anag00.TipoMovimentoMagBulk;
-import it.cnr.contab.ordmag.anag00.TipoMovimentoMagHome;
-import it.cnr.contab.ordmag.anag00.TipoOperazioneOrdBulk;
-import it.cnr.contab.ordmag.anag00.UnitaOperativaOrdBulk;
-import it.cnr.contab.ordmag.anag00.UnitaOperativaOrdHome;
-import it.cnr.contab.ordmag.ordini.bulk.OrdineAcqBulk;
+import it.cnr.contab.ordmag.anag00.*;
 import it.cnr.contab.utenze00.bp.CNRUserContext;
 import it.cnr.jada.UserContext;
 import it.cnr.jada.comp.ApplicationException;
@@ -44,6 +31,9 @@ import it.cnr.jada.persistency.PersistentCache;
 import it.cnr.jada.persistency.sql.CompoundFindClause;
 import it.cnr.jada.persistency.sql.FindClause;
 import it.cnr.jada.persistency.sql.SQLBuilder;
+
+import java.sql.Connection;
+import java.util.Optional;
 
 public class ParametriSelezioneMovimentiHome extends AbilitazioneMagazzinoHome {
 	private static final long serialVersionUID = 1L;
@@ -76,7 +66,7 @@ public class ParametriSelezioneMovimentiHome extends AbilitazioneMagazzinoHome {
 			UnitaOperativaOrdHome unitaOperativaHome, UnitaOperativaOrdBulk unitaOperativaBulk, 
 			CompoundFindClause compoundfindclause) throws PersistencyException, ComponentException{
 		if (parametri == null || parametri.getMagazzinoAbilitato() == null || parametri.getMagazzinoAbilitato().getCdMagazzino() == null){
-			throw new PersistencyException("Selezionare prima il codice del magazzino");
+			throw new ApplicationException("Selezionare prima il codice del magazzino");
 		}
 		SQLBuilder sql = unitaOperativaHome.selectByClause(userContext, compoundfindclause);
 		AbilUtenteUopOperHome home = (AbilUtenteUopOperHome) getHomeCache().getHome(AbilUtenteUopOperBulk.class);
