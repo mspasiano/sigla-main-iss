f99af942769856c32d6bbcc3d721745c97035d1e
salvio_ciro <cirosalvio@hotmail.com>
2023-12-04 11:12:56 +0100

    gestione causale variazione per fattura

--- sigla-ejb/src/main/java/it/cnr/contab/docamm00/docs/bulk/Fattura_passivaBulk.java
+++ sigla-ejb/src/main/java/it/cnr/contab/docamm00/docs/bulk/Fattura_passivaBulk.java
@@ -3325,9 +3325,11 @@ public abstract class Fattura_passivaBulk
     }
 
     public Dictionary getCausaleKeys() {
-        //if ( this.isFromAmministra()){
-        //    CAUSALE.put(NVARI,"Nota di Variazione");
-        //}
+        CAUSALE.remove(NVARI);
+        if ( ( this.isNotNew() && NVARI.equalsIgnoreCase(getCausale()))
+                ||this.isFromAmministra()){
+            CAUSALE.put(NVARI,"Nota a Variazione a Debito");
+        }
         return CAUSALE;
     }
 
@@ -3436,7 +3438,8 @@ public abstract class Fattura_passivaBulk
     }
 
     public boolean isROStato_liquidazione() {
-
+        if ( !isFromAmministra() && NVARI.equalsIgnoreCase(getCausale()))
+            return Boolean.TRUE;
         return (isGenerataDaCompenso() && getCompenso() != null);
     }
 
